5194b20a9e8b90548a7e2e0727341c23c1e047cc
cagatay <cagataycakir@gmail.com>
2021-08-04 17:30:13 +0300

    uc

--- scrapy/tutorial/tutorial/spiders/quotes_spider.py
+++ scrapy/tutorial/tutorial/spiders/quotes_spider.py
@@ -1,20 +1,39 @@
+# import scrapy
+
+# class QuotesSpider(scrapy.Spider):
+#     name = 'quotes'
+
+#     def start_request(self):
+#         urls= [
+#                 'http://quotes.toscrape.com/page/1/',
+#                 'http://quotes.toscrape.com/page/2/',
+#                 ]
+#         for url in urls:
+#             yield scrapy.Request(url=url, callback=self.parse)
+        
+#     def parse(self, response):
+#         page= response.url.split("/")[-2]
+#         filename=f'quotes-{page}.html'
+#         with open(filename, 'wb') as f:
+#             f.write(response.body)
+#         self.log(f'Saved file {filename}')
 import scrapy
 
-class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
 
-    def start_request(self):
-        urls= [
-                'http://quotes.toscrape.com/page/1/',
-                'http://quotes.toscrape.com/page/2/',
-                ]
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+
+    def start_requests(self):
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
         for url in urls:
-            yield scrapy.Request(urd=url, callback=self.parse)
-        
+            yield scrapy.Request(url=url, callback=self.parse)
+
     def parse(self, response):
-        page= response.url.split("/")[-2]
-        filename=f'quotes-{page}.html'
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log(f'Saved file {filename}')
-
